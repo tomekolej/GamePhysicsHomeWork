ddcd2638ee7bcaef72d61e217fa2838cd321b9b8
Tomasz Olejnik <t.olejnik@itmanufaktura.pl>
2017-11-10 09:46:47 +0100

    Start

---
+++ Assets/Gong.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gong : MonoBehaviour
+{
+    [SerializeField] AudioSource AudioSource;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if (!AudioSource.isPlaying)
+            AudioSource.Play();
+    }
+}
